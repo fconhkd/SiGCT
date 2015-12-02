2984354b575dbf9f8f5076a77da72eea2781bcd2
fabianohkd <fabianoconrado@gmail.com>
2015-12-01 22:38:24 -0200

    Incluindo arquivos de mapeamento faltantes

---
+++ SiGCT/Models/TipoCategoriaEnum.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace SiGCT.Models
+{
+    public enum TipoCategoriaEnum
+    {
+        [Description("Categoria de Chamadas")]
+        CHAMADA,
+
+        [Description("Categoria de Serviços")]
+        SERVICO,
+
+        [Description("Categoria de Planos")]
+        PLANO,
+
+        [Description("Categoria de Descontos")]
+        DESCONTO,
+
+        [Description("Categoria de Ajustes")]
+        AJUSTE,
+
+        [Description("Categoria de Informativos")]
+        INFORMATIVO
+    }
+}
