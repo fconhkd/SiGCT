a3ce3e2b3d1d1c49bab0d3948e8a8a3d196d72f4
fabianohkd <fabianoconrado@gmail.com>
2017-05-26 01:28:34 -0300

    Iniciando as implementações de leitura do arquivo

--- SiGCT.Data/Business/ContaBusiness.cs
+++ SiGCT.Data/Business/ContaBusiness.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
 using NHibernate.Helper.Generics;
 using SiGCT.Data.DAO;
 using SiGCT.Models;
@@ -11,6 +13,70 @@ namespace SiGCT.Data.Business
 {
     public class ContaBusiness : GenericBusiness<long, Conta, ContaDAO>
     {
+        #region Fields
+        /// <summary>
+        /// Helper para criar logs, para detalhes veja <see cref="log4net"/>
+        /// </summary>
+        private static log4net.ILog logger = LogManager.GetLogger(System.Reflection.Assembly.GetExecutingAssembly().GetName().ToString());
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Construtor padrão
+        /// </summary>
+        public ContaBusiness() { }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Metodo para ler o arquivo na versão V3R0
+        /// </summary>
+        /// <param name="path">nome do arquivo</param>
+        /// <returns></returns>
+        public bool LerArquivoV3R0(string path)
+        {
+            if (File.Exists(path))
+            {
+                using (var file = new TextFieldParser(path, Encoding.UTF8))
+                {
+                    file.TextFieldType = FieldType.FixedWidth;
+                    while (!file.EndOfData)
+                    {
+                        try
+                        {
+                            var rowType = file.PeekChars(2);
+                            switch (rowType)
+                            {
+                                case "00": break;
+                                case "10": break;
+                                case "20": break;
+                                case "30": break;
+                                case "40": break;
+                                case "50": break;
+                                case "60": break;
+                                case "70": break;
+                                case "80": break;
+                                case "90": break;
+                                case "99": break;
+                                default: //TODO : Verifica o que fazer aqui
+                                    break;
+                            }
+                        }
+                        catch (MalformedLineException ex)
+                        {
+                            logger.Error(String.Format("Erro na linha '{0}' no arquivo '{1}', a linha não está em formato valido", file.LineNumber, path), ex);
+                        }
+                        catch (Exception ex)
+                        {
+                            logger.Error(String.Format("Erro na linha '{0}' no arquivo '{1}' ", file.LineNumber, path), ex);
+                        }
+                    }
+                    return _listaBaixa;
+                }
+            }
+            return false;
+        }
+        #endregion
 
     }
 }
